c66dee4f861508d4ab1166bbdfb32ac4303342a7
Andrey Kashin <askasin@yandex.ru>
2021-11-07 20:29:06 +0700

    content-3

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -2,13 +2,19 @@
   "use strict"; // Start of use strict
 
   $(document).ready(function () {
-
-    $('.slider').slick({
+    const options = {
       infinite: true,
-      slidesToShow: 5,
       slidesToScroll: 1,
-      prevArrow:'<i class="fa fa-angle-left slick-prev" aria-hidden="true"></i>',
-      nextArrow:'<i class="fa fa-angle-right slick-next" aria-hidden="true"></i>',
+      prevArrow: '<i class="fa fa-angle-left slick-prev" aria-hidden="true"></i>',
+      nextArrow: '<i class="fa fa-angle-right slick-next" aria-hidden="true"></i>',
+    }
+    $('.slider').slick({
+      ...options,
+      slidesToShow: 5,
+    });
+    $('.slider2').slick({
+      ...options,
+      slidesToShow: 8,
     });
 
   });
